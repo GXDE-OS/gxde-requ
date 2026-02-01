4a18fb2d3a4e252289c755af83a7e95397a4c669
gfdgd_xi <3025613752@qq.com>
2026-02-01 09:52:28 +0800

    新增屏幕检测
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include "widget.h"
 #include <QApplication>
+#include <QList>
+
+QList <Widget*> list;
+
+void loadWidget() {
+    for (int i = 0; i < list.count(); ++i) {
+        delete list[i];
+        list.remove(i);
+    }
+    for (QScreen *screen: QGuiApplication::screens()) {
+        list << new Widget(screen);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // 在 Wayland 下需要使用 xwayland 运行
     qputenv("QT_QPA_PLATFORM", "xcb");
 
     QApplication a(argc, argv);
-    for (QScreen *screen: QGuiApplication::screens()) {
-        new Widget(screen);
-    }
+
+    loadWidget();
+
+    QAction::connect(&a, &QApplication::screenAdded, NULL, &loadWidget);
+    QAction::connect(&a, &QApplication::screenRemoved, NULL, &loadWidget);
+    QAction::connect(&a, &QApplication::primaryScreenChanged, NULL, &loadWidget);
 
     return a.exec();
 }
